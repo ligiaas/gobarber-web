dd352c8ab103d7f9b4480a6ed4118c2936ac2203
LIGIA DE ALMEIDA SILVA <38623489+ligiaas@users.noreply.github.com>
2020-10-10 23:25:08 -0300

    insert unform in comps

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,19 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
-  color: #666360;
+interface ContainerProps {
+  isFocused: boolean;
+  isField: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
-  border: 2px solid #232129;
   border-radius: 10px;
   padding: 16px;
   width: 100%;
+
+  color: #666360;
+  border: 2px solid #232129;
 
   display: flex;
   align-items: center;
 
   & + div {
-      margin-top: 8px;
-    }
+    margin-top: 8px;
+  }
+
+  ${props =>
+    props.isField &&
+    css`
+      color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
 
   input {
     color: #f4ede8;
@@ -28,5 +47,4 @@ export const Container = styled.div`
   svg {
     margin-right: 16px;
   }
-
 `;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-
+import { Form } from '@unform/web';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const SignIn: React.FC = () => {
+  function sendLogin(data: object): void {
+    console.log(data);
+  }
 
-      <form>
-        <h1>Faça seu logon</h1>
-        <Input name="Email" icon={FiMail} placeholder="E-mail" />
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-        <Button type="submit">Entrar</Button>
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
-      <a href="login">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
+        <Form onSubmit={sendLogin}>
+          <h1>Faça seu logon</h1>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 export default SignIn;

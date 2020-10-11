0df657e59129e995cffbac05b53adc90bd937a08
LIGIA DE ALMEIDA SILVA <38623489+ligiaas@users.noreply.github.com>
2020-10-11 00:12:55 -0300

    stylizing error display

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -23,7 +23,7 @@ const SignUp: React.FC = () => {
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'Mínimo de 6 caracteres'),
+        password: Yup.string().min(6, 'Mínimo de 6 dígitos'),
       });
 
       await schema.validate(data, {
